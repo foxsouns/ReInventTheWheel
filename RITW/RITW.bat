2b75abde7dfbb15ab258b4f0bbd122fd38d5a73d
Sage <foxsouns@gmail.com>
2021-03-11 22:09:11 -0600

    Update RITW.bat

--- RITW/RITW.bat
+++ RITW/RITW.bat
@@ -3,7 +3,6 @@ setlocal enabledelayedexpansion
 
 ::intro
 echo A front-end to the Microsoft Command Prompt. [Version 0.1.1-alpha]
-:: it's a 1.x.x update because it does what i want it to do. but its not finished: i want to put more dumb utils and stuff in.
 echo made by foxsouns using her time writing this instead of doing her schoolwork in march 2021
 echo cool new commands: ry-spec, quit
 echo. 
