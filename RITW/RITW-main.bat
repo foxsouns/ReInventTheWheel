8476e3f703d56bef46bc86f227cf7ca58823fbbc
Sage <foxsouns@gmail.com>
2021-04-06 14:40:17 -0500

    Update and rename RITW.bat to RITW-main.bat

--- RITW/RITW-main.bat
+++ RITW/RITW-main.bat
@@ -2,19 +2,34 @@
 setlocal enabledelayedexpansion
 setlocal enableextensions
 
-::bypass intro if called from addons
-if "!ckrun!"=="1" goto monke
+title ReInventTheWheel
+:: sorry! gonna need this later lol
+set initcd=!cd!
 
-::intro
+::bypass intro if already called
+if "!ckrun!"=="1" (
+	goto monke 
+) else (
+	set ckrun=1
+	goto intro
+)
+
+:intro
 echo A front-end to the Microsoft Command Prompt. [Version 0.1.3-alpha]
 echo made by foxsouns using her time writing this instead of doing her schoolwork in march-april 2021
 echo.
 echo WARNIN': this thingy depends on the names being the right thing, 
 echo and that theyre being ran in the same folder. else: addons wont work.
 echo. 
-echo. youve got "quit", and if its workin, you have access to ry-spec. have fun!
-:: sorry! gonna need this later lol
-set initcd=!cd!
+echo the script's got "quit", lookin for ry-spec now. 
+echo.
+
+if exist "!initcd!\RITW-addons.bat" (
+echo yup! you're good to go. run "ry-spec" for some neat shit lol
+) else (
+echo sorry mate, you don't have RITW-addons.bat.
+)
+echo.
 
 :: BE MONKE
 :monke
